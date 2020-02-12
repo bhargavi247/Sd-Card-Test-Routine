28c981bf8189215ec51fc11057bddf0eb1faeaea
bhargavi <bhargavi@umich.edu>
2020-02-12 00:05:38 -0500

    Comments Updated

--- writeRoutine/writeRoutine.ino
+++ writeRoutine/writeRoutine.ino
@@ -1,10 +1,9 @@
 #include <SPI.h>
-//#include <SD.h>
 #include "SdFat.h"
 SdFat sd;
 
 #define SPI_SPEED SD_SCK_MHZ(4)
-#define chipSelect 9//4
+#define chipSelect 9
 #define buttonPin 6
 #define ledPin 8
 #define FILE_BASE_NAME "BR_"
@@ -13,20 +12,22 @@ const unsigned long period = 10000;
 unsigned long startMillis;
 int buttonState = 0;
 
-void setup() {
-  // Open serial communications and wait for port to open:
+void setup(){
+  // Open serial communications and wait for port to open.
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT); // initialize the LED pin as an output:
-  pinMode(buttonPin, INPUT_PULLUP);  // initialize the pushbutton pin as an input:
-  //  attachInterrupt(digitalPinToInterrupt(buttonPin), writeSD, CHANGE);
-  while (!Serial) {}
+  pinMode(ledPin, OUTPUT); // initialize the LED pin as an output.
+  pinMode(buttonPin, INPUT_PULLUP); // initialize the pushbutton pin as an input_pullup.
+  while (!Serial)
+  {}
 
   Serial.print("Initializing SD card...");
-  if (!sd.begin(chipSelect, SPI_SPEED)) {
+  if (!sd.begin(chipSelect, SPI_SPEED)){ //sd.begin() initializes the SD card after which the Sd card goes into sleep mode
     Serial.println("initialization failed!");
-    return; // This return statement terminates the program.
+    return; // If initialization of Sd card fails, the program is terminated
   }
   Serial.println("initialization done.");
+
+  //Led blinks twice indicating that initialization of Sd card is successful
   digitalWrite(ledPin, LOW);
   delay(200);
   digitalWrite(ledPin, HIGH);
@@ -38,30 +39,31 @@ void setup() {
   digitalWrite(ledPin, LOW);
 }
 
-void loop() {
-  if (!digitalRead(buttonPin)) {
+void loop(){
+  if (!digitalRead(buttonPin)){
     writeSD();
   }
 }
 
-void writeSD() {
-  //  detachInterrupt(digitalPinToInterrupt(buttonPin));
+void writeSD(){
   // Find an unused file name.
   const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
   char fileName[13] = FILE_BASE_NAME "00.txt";
-  while (sd.exists(fileName)) {
-    if (fileName[BASE_NAME_SIZE + 1] != '9') {
+  while (sd.exists(fileName)){
+    if (fileName[BASE_NAME_SIZE + 1] != '9'){
       fileName[BASE_NAME_SIZE + 1]++;
-    } else if (fileName[BASE_NAME_SIZE] != '9') {
+    }
+    else if (fileName[BASE_NAME_SIZE] != '9'){
       fileName[BASE_NAME_SIZE + 1] = '0';
       fileName[BASE_NAME_SIZE]++;
     }
   }
   Serial.println("ISR entered...");
+  //the sd card is in write mode
   startMillis = millis();
-  if (file.open(fileName, O_WRONLY | O_CREAT | O_EXCL)) {
+  if (file.open(fileName, O_WRONLY | O_CREAT | O_EXCL)){
     digitalWrite(ledPin, HIGH);
-    while (millis() - startMillis < period) {
+    while (millis() - startMillis < period){ //Write the character 'X' for 10 seconds
       file.print("X");
     }
     file.close();
@@ -69,10 +71,9 @@ void writeSD() {
     Serial.println("done.");
   }
   // close the file:
-  else {
+  else{
     // if the file didn't open, print an error:
     Serial.println("error opening file");
   }
   delay(1000); // switch debounce in case SD doesn't open
-  //  attachInterrupt(digitalPinToInterrupt(buttonPin), writeSD, CHANGE);
 }
